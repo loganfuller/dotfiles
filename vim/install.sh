4102f3ca58d06a2e22860d3b3e2cb5ff89df44f7
Logan Fuller <fuller.logan@gmail.com>
2014-07-15 19:34:38 -0400

    now using curl to install vim solarized colors

--- vim/install.sh
+++ vim/install.sh
@@ -8,5 +8,5 @@
 [[ -d ~/.vim/colors ]] || mkdir -p ~/.vim/colors
 if ! [ -f ~/.vim/colors/solarized.vim ]
 then
-    wget --no-check-certificate https://raw.githubusercontent.com/altercation/solarized/master/vim-colors-solarized/colors/solarized.vim -O ~/.vim/colors/solarized.vim
+    curl -L -o ~/.vim/colors/solarized.vim https://raw.githubusercontent.com/altercation/solarized/master/vim-colors-solarized/colors/solarized.vim
 fi
